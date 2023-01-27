14a54fa105fe9f702bb52d6578f47a461002694f
Андрей Иванов <andreyproject97@gmail.com>
2023-01-27 22:17:42 +0300

    hw4

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+
+module.exports = {
+    entry: './src/cardgame.js',
+    mode: 'production',
+    module: {
+        rules: [
+            { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        clean: true,
+    },
+    plugins: [
+        new CopyPlugin({
+            patterns: [{ from: 'static', to: 'static' }],
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './index.html',
+        }),
+        new MiniCssExtractPlugin(),
+        new HtmlWebpackPlugin(),
+    ],
+};
